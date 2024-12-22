f9892057225a61a09b4f2696ad8532800ab92548
Marczelloo <moskwamarcel@gmail.com>
2024-12-22 18:00:44 +0100

    Initial commit, Project setup, Landing Page

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app  = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+require('dotenv').config();
+
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(session({
+   secret: process.env.SESSION_SECRET,
+   resave: false,
+   saveUninitialized: false,
+   cookie: { 
+      secure: false,
+      maxAge: 60 * 60 * 1000
+   }
+}))
+
+const pageRoutes = [
+   { path: '/', route: require('./routers/landingPage.js')}
+]
+
+pageRoutes.forEach(route => {
+   app.use(route.path, route.route);
+})
+
+app.listen(PORT, () => {
+   console.log(`Server is running on port ${PORT}`);
+   console.log(`http://localhost:${PORT}`);
+});
---
+++ public/scripts/theme-toggle.js
@@ -0,0 +1,17 @@
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+toggleButton.addEventListener('click', () => {
+   if(body.classList.contains('dark-mode'))
+   {
+      body.classList.remove('dark-mode');
+      body.classList.add('light-mode');
+   }
+   else
+   {
+      body.classList.remove('light-mode');
+      body.classList.add('dark-mode');
+   }
+});
+
+body.classList.add('dark-mode');
